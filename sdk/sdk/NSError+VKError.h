69cf3dc75604bef97a5b982e961afe63308e08f9
Roman Truba <dreddkr@gmail.com>
2014-01-28 14:09:59 +0400

    Updated description

--- sdk/sdk/NSError+VKError.h
+++ sdk/sdk/NSError+VKError.h
@@ -10,12 +10,23 @@
 #import "VKError.h"
 
 static NSString *const VKSdkErrorDomain = @"VKSdkErrorDomain";
-static NSString *VkErrorDescriptionKey      = @"VkErrorDescriptionKey";
+static NSString *VkErrorDescriptionKey  = @"VkErrorDescriptionKey";
 @interface NSError (VKError)
 
+/// Returns vk error associated with that NSError
 @property (nonatomic, readonly)  VKError * vkError;
 
+/**
+ Create new NSError with VKError
+ @param vkError Source error
+ @return New error with VKSdkErrorDomain domain
+ */
 +(NSError*) errorWithVkError:(VKError*) vkError;
+/**
+ Copies user info from this NSError into new error, with adding VKError
+ @param vkError Source error
+ @return New error with this error domain, code and user info
+ */
 -(NSError*) copyWithVkError:(VKError*) vkError;
 
 @end
