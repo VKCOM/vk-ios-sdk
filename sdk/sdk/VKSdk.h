e7788bd8ade7ce12f44fdde362c5b9dd51d1113f
Roman Truba <dreddkr@gmail.com>
2014-04-25 18:47:11 +0400

    Update VKSdk.h

--- sdk/sdk/VKSdk.h
+++ sdk/sdk/VKSdk.h
@@ -146,30 +146,30 @@
  */
 + (void)authorize:(NSArray *)permissions revokeAccess:(BOOL)revokeAccess;
 /**
- Starts authorization process. If VKapp is available in system, it will opens and requests access from user.
+ Starts authorization process.
  Otherwise Mobile Safari will be opened for access request.
  @param permissions Array of permissions for your applications. All permissions you can
  @param revokeAccess If YES, user will allow logout (to change user)
- @param forceOAuth SDK will use only oauth authorization, through uiwebview
+ @param forceOAuth If YES, SDK will use only oauth authorization through mobile safari. Otherwise, it will try to authorize through VK application
  */
 + (void)authorize:(NSArray *)permissions revokeAccess:(BOOL)revokeAccess forceOAuth:(BOOL)forceOAuth;
 
 /**
- Starts authorization process. If VKapp is available in system, it will opens and requests access from user.
+ Starts authorization process.
  Otherwise Mobile Safari will be opened for access request.
  @param permissions Array of permissions for your applications. All permissions you can
  @param revokeAccess If YES, user will allow logout (to change user)
- @param forceOAuth SDK will use only oauth authorization, through uiwebview
+ @param forceOAuth If YES, SDK will use only oauth authorization through mobile safari. Otherwise, it will try to authorize through VK application
  @param inApp If YES, SDK will try to open modal window with webview to authorize. This method strongly not recommended as user should enter his account data in your application. For use modal view add VKSdkResources.bundle to your project.
  */
 + (void)authorize:(NSArray *)permissions revokeAccess:(BOOL)revokeAccess forceOAuth:(BOOL)forceOAuth inApp:(BOOL) inApp;
 
 /**
- Starts authorization process. If VKapp is available in system, it will opens and requests access from user.
+ Starts authorization process. 
  Otherwise Mobile Safari will be opened for access request.
  @param permissions Array of permissions for your applications. All permissions you can
  @param revokeAccess If YES, user will allow logout (to change user)
- @param forceOAuth SDK will use only oauth authorization, through uiwebview
+ @param forceOAuth If YES, SDK will use only oauth authorization through mobile safari. Otherwise, it will try to authorize through VK application
  @param inApp If YES, SDK will try to open modal window with webview to authorize. This method strongly not recommended as user should enter his account data in your application. For use modal view add VKSdkResources.bundle to your project.
  @param displayType Defines view of authorization screen
  */
