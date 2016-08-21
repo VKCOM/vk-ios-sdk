891dd8ec2386327b73f537620e06792d52c1a02f
Richard <richardot4@gmail.com>
2016-08-21 17:18:38 +0300

    Add VKPoll model

---
+++ library/Source/API/models/VKPoll.h
@@ -0,0 +1,49 @@
+//
+//  VKPoll.h
+//
+//  Copyright (c) 2016 VK.com
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import "VKApiObjectArray.h"
+
+@interface VKPollAnswer : VKApiObject
+
+@property(nonatomic, strong) NSNumber *id;
+@property(nonatomic, strong) NSString *text;
+@property(nonatomic, strong) NSNumber *votes;
+@property(nonatomic, strong) NSNumber *rate;
+
+@end
+
+@interface VKPollAnswers : VKApiObjectArray
+@end
+
+@interface VKPoll : VKApiObject
+
+@property(nonatomic, strong) NSNumber *id;
+@property(nonatomic, strong) NSNumber *owner_id;
+@property(nonatomic, strong) NSNumber *from_id;
+@property(nonatomic, strong) NSNumber *created;
+@property(nonatomic, strong) NSString *question;
+@property(nonatomic, strong) NSNumber *votes;
+@property(nonatomic, strong) NSNumber *answer_id;
+@property(nonatomic, strong) VKPollAnswers *answers;
+@property(nonatomic, strong) NSNumber *anonymous;
+
+@end
