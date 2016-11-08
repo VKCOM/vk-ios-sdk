3ac4f0bcead007d295073238b3b9e52b630b7ff9
Oleg Suzhaev <oleg.suzhaev+github@gmail.com>
2016-11-08 19:55:54 +0300

    Fixed #458

--- library/Source/VKAccessToken.m
+++ library/Source/VKAccessToken.m
@@ -230,7 +230,8 @@ static NSString *const PERMISSIONS = @"permissions";
     NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
     SecItemDelete((__bridge CFDictionaryRef) keychainQuery);
     keychainQuery[(__bridge id) kSecValueData] = [NSKeyedArchiver archivedDataWithRootObject:token];
-    SecItemAdd((__bridge CFDictionaryRef) keychainQuery, NULL);
+    OSStatus status = SecItemAdd((__bridge CFDictionaryRef) keychainQuery, NULL);
+    NSAssert(status == errSecSuccess, @"Unable to store VKAccessToken in keychain. OSStatus: %i. Error Description: https://www.osstatus.com/search/results?platform=all&framework=all&search=%i or https://developer.apple.com/reference/security/1658642-keychain_services", status, status);
 }
 
 + (VKAccessToken *)load:(NSString *)service {
